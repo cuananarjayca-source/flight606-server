5cf5a3092218f8d13e48b0266eb1980183b6b4df
cuananarjayca-source <cuananarjay.ca@gmail.com>
2026-06-29 20:26:14 +0800

    Fix: Enhance getMyBookingsUser to populate flight details

--- controllers/booking.js
+++ controllers/booking.js
@@ -172,6 +172,14 @@ module.exports.createBookingGuest = (req, res) => {
 
 module.exports.getMyBookingsUser = (req, res) => {
     return Booking.find({ userId: req.user.id })
+        .populate({
+            path: 'flightId',
+            populate: [
+                { path: 'originAirportId' },
+                { path: 'destinationAirportId' },
+                { path: 'airlineId' }
+            ]
+        })
         .then(result => {
             if (result.length === 0) {
                 return res.status(404).send({ message: "No bookings found" });
